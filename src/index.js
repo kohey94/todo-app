bcb355799a360e0a115e9013749b271097439c32
kohey94 <sugawara622@gmail.com>
2024-08-22 00:58:25 +0900

    create todoapp

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const DATA = [
+  {id: "todo-0", name: "食事", completed: true},
+  {id: "todo-1", name: "睡眠", completed: false},
+  {id: "todo-2", name: "Repert", completed: false},
+];
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA}/>
   </React.StrictMode>
 );
 
