0f0c8b18bfda10bf8ca722b57cf5d4d37202e699
kohey94 <sugawara622@gmail.com>
2024-08-22 12:48:17 +0900

    フィルター処理追加

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ import Todo from "./components/Todo";
 import { useState } from "react";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
+  const [filter, setFilter] = useState("All"); 
+  
+
   const [tasks, setTasks] = useState(props.tasks);
 
   function toggleTaskCompleted(id) {
@@ -36,15 +45,26 @@ function App(props) {
     setTasks(editedTaskList);
   }
 
-  const taskList = tasks?.map((task) => (
-    <Todo 
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-      editTask={editTask}
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo 
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+  ));
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton 
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
     />
   ));
 
@@ -62,9 +82,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
