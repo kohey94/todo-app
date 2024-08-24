796e0515f64edca25a076598e658bec2de4a0c2e
kohey94 <sugawara622@gmail.com>
2024-08-25 02:45:52 +0900

    authテスト追加

--- backend/tests/auth.test.js
+++ backend/tests/auth.test.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = require('../server');
 const User = require('../models/User');
 
-require('dotenv').config({ path: '../.env' }); // パスを修正
+require('dotenv').config({ path: '../.env' });
 
 beforeAll(async () => {
     console.log('JWT_SECRET:', process.env.JWT_SECRET); // 環境変数を確認
@@ -20,12 +20,50 @@ afterAll(async () => {
 });
 
 describe('AuthAPIテスト', () => {
-    it('ユーザー登録', async () => {
-        const response = await request(app)
-            .post('/api/register')
-            .send({ username: 'testuser', password: 'testpassword'});
+  it('ユーザー登録', async () => {
+    const response = await request(app)
+      .post('/api/register')
+      .send({ username: 'testuser', password: 'testpassword'});
         
-            expect(response.statusCode).toBe(201);
-            expect(response.body).toHaveProperty('message', 'User registered successfully');
-    });
+      expect(response.statusCode).toBe(201);
+      expect(response.body).toHaveProperty('message', 'User registered successfully');
+  });
+
+  it('重複ユーザー登録', async () => {
+    const response = await request(app)
+    .post('/api/register')
+    .send({ username: 'testuser', password: 'testpassword'});
+    
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty('message', 'Username already exists');
+  });
+
+  it('ユーザーログイン', async () => {
+    const response = await request(app)
+      .post('/api/login')
+      .send({ username: 'testuser', password: 'testpassword'});
+    
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty('token');
+    expect(response.body).toHaveProperty('username', 'testuser');
+  });
+
+  it('ユーザーログイン失敗', async () => {
+    const response = await request(app)
+      .post('/api/login')
+      .send({ username: 'testuser', password: 'testpassword2'});
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body).toHaveProperty('message', 'Invalid credentials');
+  });
+
+  it('ユーザーログイン失敗2', async () => {
+    const response = await request(app)
+      .post('/api/login')
+      .send({ username: 'testuser2', password: 'testpassword'});
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body).toHaveProperty('message', 'Invalid credentials');
+  });
+
 });
