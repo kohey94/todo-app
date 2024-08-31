b6751b7c589c6240f3f90649fb92c07e57892a18
kohey94 <sugawara622@gmail.com>
2024-09-01 06:12:40 +0900

    テスト通ったが、通らないこともある

--- backend/tests/todo.test.js
+++ backend/tests/todo.test.js
@@ -8,14 +8,24 @@ const app = require('../app');
 
 require('dotenv').config();
 
+let tokenA;
+let tokenB;
+
 beforeAll(async () => {
     // テスト用のMongoDB URIで接続
   await mongoose.connect(process.env.MONGODB_TEST_URI);
 
   // テストユーザーの作成
-  const hashedPassword = await bcrypt.hash('testpassword', 10);
-  const user = new User({ username: 'testtodouser', password: hashedPassword });
-  await user.save();
+  const hashedPassA = await bcrypt.hash('testpassA', 10);
+  const userA = new User({ username: 'userA', password: hashedPassA });
+  await userA.save();
+  tokenA = jwt.sign({ userId: userA._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  
+  // テストユーザーの作成
+  const hashedPassB = await bcrypt.hash('testpassB', 10);
+  const userB = new User({ username: 'userB', password: hashedPassB });
+  await userB.save();
+  tokenB = jwt.sign({ userId: userB._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
   
   
 });
@@ -32,7 +42,7 @@ describe('Todo API Tests', () => {
     it('should get an empty todo list initially', async () => {
         const response = await request(app)
             .get('/api/todos')
-            //.set('Authorization', `Bearer ${token}`)
+            .set('Authorization', `Bearer ${tokenA}`)
             ;
     
         expect(response.statusCode).toBe(200);
@@ -43,7 +53,7 @@ describe('Todo API Tests', () => {
     it('should create a new todo', async () => {
         const response = await request(app)
             .post('/api/todos')
-            //.set('Authorization', `Bearer ${token}`)
+            .set('Authorization', `Bearer ${tokenA}`)
             .send({ name: 'Test Todo' });
     
         expect(response.statusCode).toBe(200);
@@ -54,7 +64,7 @@ describe('Todo API Tests', () => {
     it('should get a list of todos', async () => {
         const response = await request(app)
             .get('/api/todos')
-            //.set('Authorization', `Bearer ${token}`)
+            .set('Authorization', `Bearer ${tokenA}`)
             ;
     
         expect(response.statusCode).toBe(200);
@@ -66,14 +76,14 @@ describe('Todo API Tests', () => {
     it('should update a todo', async () => {
         const todos = await request(app)
             .get('/api/todos')
-            //.set('Authorization', `Bearer ${token}`)
+            .set('Authorization', `Bearer ${tokenA}`)
             ;
     
         const todoId = todos.body[0]._id;
     
         const response = await request(app)
             .put(`/api/todos/${todoId}`)
-            //.set('Authorization', `Bearer ${token}`)
+            .set('Authorization', `Bearer ${tokenA}`)
             .send({ name: 'Updated Todo', completed: true });
     
         expect(response.statusCode).toBe(200);
@@ -84,14 +94,14 @@ describe('Todo API Tests', () => {
     it('should delete a todo', async () => {
         const todos = await request(app)
             .get('/api/todos')
-            //.set('Authorization', `Bearer ${token}`)
+            .set('Authorization', `Bearer ${tokenA}`)
             ;
     
         const todoId = todos.body[0]._id;
     
         const response = await request(app)
             .delete(`/api/todos/${todoId}`)
-            //.set('Authorization', `Bearer ${token}`)
+            .set('Authorization', `Bearer ${tokenA}`)
             ;
     
         expect(response.statusCode).toBe(200);
@@ -99,64 +109,34 @@ describe('Todo API Tests', () => {
     });
 
     
-    // it('should show User A\'s todo only to User A and not to User B', async () => {
-    //     // ユーザーAの登録
-    //     const registerResponseA = await request(app)
-    //         .post('/api/auth/register')
-    //         .send({ username: 'userA', password: 'passwordA' });
+    it('should show User A\'s todo only to User A and not to User B', async () => {
+        // ユーザーAとしてTodoを作成
+        const createResponseA = await request(app)
+            .post('/api/todos')
+            .set('Authorization', `Bearer ${tokenA}`)
+            .send({ name: 'User A Todo' });
     
-    //     expect(registerResponseA.statusCode).toBe(201); // 登録成功のステータスコード
+        expect(createResponseA.statusCode).toBe(200);
+        expect(createResponseA.body).toHaveProperty('name', 'User A Todo');
     
-    //     // ユーザーAでログインしてトークンを取得
-    //     const loginResponseA = await request(app)
-    //         .post('/api/auth/login')
-    //         .send({ username: 'userA', password: 'passwordA' });
-    
-    //     expect(loginResponseA.statusCode).toBe(200); // ログイン成功のステータスコード
-    //     const tokenA = loginResponseA.body.token; // トークンを取得
-    
-    //     // ユーザーAとしてTodoを作成
-    //     const createResponseA = await request(app)
-    //         .post('/api/todos')
-    //         .set('Authorization', `Bearer ${tokenA}`)
-    //         .send({ name: 'User A Todo' });
-    
-    //     expect(createResponseA.statusCode).toBe(200);
-    //     expect(createResponseA.body).toHaveProperty('name', 'User A Todo');
-    
-    //     // ユーザーAのTodoがユーザーAのリストに存在することを確認
-    //     const getTodosResponseA = await request(app)
-    //         .get('/api/todos')
-    //         .set('Authorization', `Bearer ${tokenA}`);
+        // ユーザーAのTodoがユーザーAのリストに存在することを確認
+        const getTodosResponseA = await request(app)
+            .get('/api/todos')
+            .set('Authorization', `Bearer ${tokenA}`);
         
-    //     expect(getTodosResponseA.statusCode).toBe(200);
-    //     expect(Array.isArray(getTodosResponseA.body)).toBe(true);
-    //     expect(getTodosResponseA.body.length).toBe(1);
-    //     expect(getTodosResponseA.body[0]).toHaveProperty('name', 'User A Todo');
+        expect(getTodosResponseA.statusCode).toBe(200);
+        expect(Array.isArray(getTodosResponseA.body)).toBe(true);
+        expect(getTodosResponseA.body.length).toBe(1);
+        expect(getTodosResponseA.body[0]).toHaveProperty('name', 'User A Todo');
     
-    //     // ユーザーBの登録
-    //     const registerResponseB = await request(app)
-    //         .post('/api/auth/register')
-    //         .send({ username: 'userB', password: 'passwordB' });
+        // ユーザーBとしてTodoリストを取得し、ユーザーAのTodoが見えないことを確認
+        const getTodosResponseB = await request(app)
+            .get('/api/todos')
+            .set('Authorization', `Bearer ${tokenB}`);
     
-    //     expect(registerResponseB.statusCode).toBe(201); // 登録成功のステータスコード
-    
-    //     // ユーザーBでログインしてトークンを取得
-    //     const loginResponseB = await request(app)
-    //         .post('/api/auth/login')
-    //         .send({ username: 'userB', password: 'passwordB' });
-    
-    //     expect(loginResponseB.statusCode).toBe(200); // ログイン成功のステータスコード
-    //     const tokenB = loginResponseB.body.token; // トークンを取得
-    
-    //     // ユーザーBとしてTodoリストを取得し、ユーザーAのTodoが見えないことを確認
-    //     const getTodosResponseB = await request(app)
-    //         .get('/api/todosj')
-    //         .set('Authorization', `Bearer ${tokenB}`);
-    
-    //     expect(getTodosResponseB.statusCode).toBe(200);
-    //     expect(Array.isArray(getTodosResponseB.body)).toBe(true);
-    //     expect(getTodosResponseB.body.length).toBe(0); // ユーザーBにはユーザーAのTodoが見えないことを確認
-    // });
+        expect(getTodosResponseB.statusCode).toBe(200);
+        expect(Array.isArray(getTodosResponseB.body)).toBe(true);
+        expect(getTodosResponseB.body.length).toBe(0); // ユーザーBにはユーザーAのTodoが見えないことを確認
+    });
 
 });
